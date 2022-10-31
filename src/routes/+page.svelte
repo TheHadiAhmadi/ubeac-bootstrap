99a0fa35e0fe22263a7d93a5604a9ec99bea7873
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-10-31 15:26:44 +0430

    initialize bootstrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="grid text-center">
+  <div class="g-col-4">.g-col-4</div>
+  <div class="g-col-4">.g-col-4</div>
+  <div class="g-col-4">.g-col-4</div>
+</div>
---
+++ src/routes/notes/+page.svelte
@@ -0,0 +1,35 @@
+<h1>Styled / UnStyled css</h1>
+
+<ul>
+  <li>Button</li>
+  <li>Input</li>
+  <li>Badge</li>
+  <li>...</li>
+</ul>
+
+<br />
+<ul>
+  <li>Container</li>
+  <li>Row</li>
+  <li>Col</li>
+  <li>...</li>
+</ul>
+
+<h2>
+  <a href="https://getbootstrap.com/docs/5.2/content/reboot/#css-variables">
+    CSS Variables
+  </a>
+</h2>
+<span class="badge bg-success">Added in v5.2</span>
+<p>
+  CSS variables Added in v5.2.0 With v5.1.1, we standardized our required
+  @imports across all our CSS bundles (including bootstrap.css,
+  bootstrap-reboot.css, and bootstrap-grid.css) to include _root.scss. This adds
+  :root level CSS variables to all bundles, regardless of how many of them are
+  used in that bundle. Ultimately Bootstrap 5 will continue to see more CSS
+  variables added over time, in order to provide more real-time customization
+  without the need to always recompile Sass. Our approach is to take our source
+  Sass variables and transform them into CSS variables. That way, even if you
+  don’t use CSS variables, you still have all the power of Sass. This is still
+  in-progress and will take time to fully implement.
+</p>
